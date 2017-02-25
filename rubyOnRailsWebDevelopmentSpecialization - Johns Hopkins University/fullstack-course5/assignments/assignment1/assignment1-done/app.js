287d2e6fce4a1135eaad7f1be80d83e16cf16b0c
Samuel Barbosa <samu101108@gmail.com>
2017-02-25 02:00:01 -0300

    Solution LunchCheck

--- rubyOnRailsWebDevelopmentSpecialization - Johns Hopkins University/fullstack-course5/assignments/assignment1/assignment1-done/app.js
+++ rubyOnRailsWebDevelopmentSpecialization - Johns Hopkins University/fullstack-course5/assignments/assignment1/assignment1-done/app.js
@@ -10,24 +10,31 @@ angular.module('LunchCheck', [])
       }
       function isItMuch(){
         // feijão, arroz, macarrão, carne
+        var quantity = "";
         var s = $scope.plate;
+        console.log("THIS IS: " + s);
         // var nonAlpha = s.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
         //   console.log("nonAlpha = " + nonAlpha);
         // var nonSpace= nonAlpha.replace(/\s/g," ");
         //   console.log("nonSpace = " + nonSpace);
         // s = nonSpace;
         // s = nonAlpha;
+        if (s.length < 1){
+          $scope.message = "Please enter data first";
+            quantity = $scope.message;
+        }
         s = s.split(",");
           // console.log("split = " + s);
           // console.log(s.length);
-        if (s.length <= 3){
-        $scope.message = "It is just a bit.";
-        var quantity = $scope.message;
-      }
-        else {
-          $scope.message = "It is much!";
-          var quantity = $scope.message;
-        }
+          if(s.length >= 1  && s.length <= 3){
+            $scope.message = "Enjoy!";
+            quantity = $scope.message;
+          }
+          if (s.length > 3){
+            $scope.message = "Too much!";
+            quantity = $scope.message;
+          }
+console.log("quantity = " + quantity);
         return quantity;
       };
     });
